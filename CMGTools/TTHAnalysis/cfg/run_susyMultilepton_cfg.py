d73b06c6c5f6077a74dd876607923e46f1dd6b28
gael perrin <gael.ludovic.perrin@cern.ch>
2015-07-07 18:09:59 +0200

    Added modification to produce from DY 50ns

--- CMGTools/TTHAnalysis/cfg/run_susyMultilepton_cfg.py
+++ CMGTools/TTHAnalysis/cfg/run_susyMultilepton_cfg.py
@@ -160,9 +160,7 @@ from CMGTools.TTHAnalysis.samples.samples_13TeV_74X import *
 from CMGTools.TTHAnalysis.samples.samples_13TeV_74X_susySignalsPriv import *
 from CMGTools.TTHAnalysis.samples.samples_8TeVReReco_74X import *
 
-selectedComponents = [ SingleMu_742, MuEG_742, DoubleMu_742 ] 
-selectedComponents = [ TTJets, TTJets_LO, WJetsToLNu, DYJetsToLL_M10to50,  DYJetsToLL_M50,  ] + SingleTop + DiBosons
-selectedComponents = mcSamplesPriv 
+selectedComponents = [DYJetsToLL_M50_50ns ] 
 
 if False: # select only a subset of a sample, corresponding to a given luminosity (assuming ~30k events per MiniAOD file, which is ok for central production)
     target_lumi = 5000 # in inverse picobarns
